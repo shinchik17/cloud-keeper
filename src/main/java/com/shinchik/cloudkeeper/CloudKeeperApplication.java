5cea83534b1209ae8aeb7d526fab8ac0d74fbfd8
shinchik17 <shinyaev.2013@mail.ru>
2024-10-29 12:04:28 +0300

    init commit

---
+++ src/main/java/com/shinchik/cloudkeeper/CloudKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.shinchik.cloudkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudKeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shinchik/cloudkeeper/CloudKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinchik.cloudkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
