b0d688655db7988e79868aba0f6aa3ddb68bfce0
shinchik17 <shinyaev.2013@mail.ru>
2024-10-31 20:59:06 +0300

    feat: add global exception handler and customize error page

---
+++ src/main/java/com/shinchik/cloudkeeper/aop/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.shinchik.cloudkeeper.aop;
+
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public String handleNoResourceFound(NoResourceFoundException e, Model model){
+        String errorMessage = "Status %d: resource /%s not found.".formatted(e.getBody().getStatus(), e.getResourcePath());
+        logger.warn(errorMessage);
+        model.addAttribute("errorMessage", errorMessage);
+        return "error";
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public String handleOtherExceptions(Exception e, Model model, HttpServletRequest request){
+        request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        model.addAttribute("errorMessage", e.getMessage());
+        model.addAttribute("errorCode", e.getMessage());
+        return "error";
+    }
+
+}
