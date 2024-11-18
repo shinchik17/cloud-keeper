c1e666b91dc707a4bbf89a39ed5dc070c7dea519
shinchik17 <shinyaev.2013@mail.ru>
2024-11-18 19:54:10 +0300

    feat: add redis session store with JSON serialization

---
+++ src/main/java/com/shinchik/cloudkeeper/security/SessionConfig.java
@@ -0,0 +1,50 @@
+package com.shinchik.cloudkeeper.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shinchik.cloudkeeper.user.model.User;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
+
+@Configuration
+public class SessionConfig implements BeanClassLoaderAware {
+
+    private ClassLoader loader;
+
+    /**
+     * Note that the bean name for this bean is intentionally
+     * {@code springSessionDefaultRedisSerializer}. It must be named this way to override
+     * the default {@link RedisSerializer} used by Spring Session.
+     */
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer(objectMapper());
+    }
+
+    /**
+     * Customized {@link ObjectMapper} to add mix-in for class that doesn't have default
+     * constructors
+     * @return the {@link ObjectMapper} to use
+     */
+    private ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModules(SecurityJackson2Modules.getModules(this.loader));
+        mapper.addMixIn(SecurityUserDetails.class, SecurityUserDetails.class);
+        mapper.addMixIn(User.class, User.class);
+        return mapper;
+    }
+
+    /*
+     * @see
+     * org.springframework.beans.factory.BeanClassLoaderAware#setBeanClassLoader(java.lang
+     * .ClassLoader)
+     */
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.loader = classLoader;
+    }
+
+}
