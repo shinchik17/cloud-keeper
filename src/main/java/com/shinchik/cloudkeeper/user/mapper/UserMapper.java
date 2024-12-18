9a7e2f11be2873f2d594713b3b316c6e67033a79
shinchik17 <shinyaev.2013@mail.ru>
2024-12-18 20:15:50 +0300

    fix: warning about unmapped id target field

--- src/main/java/com/shinchik/cloudkeeper/user/mapper/UserMapper.java
+++ src/main/java/com/shinchik/cloudkeeper/user/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.shinchik.cloudkeeper.user.mapper;
 import com.shinchik.cloudkeeper.user.model.User;
 import com.shinchik.cloudkeeper.user.model.UserDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -10,6 +11,7 @@ public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "id", ignore = true)
     User mapToEntity(UserDto userDto);
 
 }
